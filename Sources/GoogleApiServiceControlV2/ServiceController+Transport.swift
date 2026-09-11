3c94943a927fbabf043622c92e1ca57770bcddfa
Carlos O'Ryan <coryan@google.com>
2026-09-11 19:52:59 +0000

    chore: update librarian with `setBody()` (#819)

--- Sources/GoogleApiServiceControlV2/ServiceController+Transport.swift
+++ Sources/GoogleApiServiceControlV2/ServiceController+Transport.swift
@@ -48,7 +48,7 @@ extension Clients {
       var req = try await self.inner.newRequest(path: path, query: query)
       req.setMethod(.POST)
       req.addHeader(name: GoogleCloudGax._HeaderNames.apiClient, value: Clients.clientHeader)
-      req.setBody(data: try JSONEncoder().encode(request), ofContentType: "application/json")
+      try req.setBody(json: request)
       return try await req.rpc(
         GoogleApiServiceControlV2.CheckResponse.self, timeout: options.attemptTimeout
       ).get()
@@ -70,7 +70,7 @@ extension Clients {
       var req = try await self.inner.newRequest(path: path, query: query)
       req.setMethod(.POST)
       req.addHeader(name: GoogleCloudGax._HeaderNames.apiClient, value: Clients.clientHeader)
-      req.setBody(data: try JSONEncoder().encode(request), ofContentType: "application/json")
+      try req.setBody(json: request)
       return try await req.rpc(
         GoogleApiServiceControlV2.ReportResponse.self, timeout: options.attemptTimeout
       ).get()
